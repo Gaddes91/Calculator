76cb724097d0789fed31982edee9659e6240136f
Matthew Gaddes <matthew.gaddes@gmail.com>
2015-12-06 17:33:35 +0000

    v0.1 Complete up to the end of Lecture 1 1. Update UILabel to show the correct digit (0-9) when the respective UIButton is pressed

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var display: UILabel!
+    
+    var userHasAlreadyTypedANumber = false
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if userHasAlreadyTypedANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userHasAlreadyTypedANumber = true
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +36,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
